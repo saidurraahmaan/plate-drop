ebbffea03e2fb8adf202d9b523e16a419418fd9d
saidurraahmaan <saidurraahmaan@gmail.com>
2024-04-14 16:50:42 +0600

    Initialzied spring project

---
+++ plate-drop-main-service/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "plate-drop-main-service"
---
+++ plate-drop-main-service/src/test/java/com/s4r/platedropmainservice/PlateDropMainServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.s4r.platedropmainservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateDropMainServiceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
