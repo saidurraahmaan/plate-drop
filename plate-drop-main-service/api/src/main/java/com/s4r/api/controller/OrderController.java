f3614a03e0e5685e0d9b8b12fe687c5fb33a2b79
saidurraahmaan <saidurraahmaan@gmail.com>
2024-05-18 22:23:36 +0600

    Ongoing

--- plate-drop-main-service/api/src/main/java/com/s4r/api/controller/OrderController.java
+++ plate-drop-main-service/api/src/main/java/com/s4r/api/controller/OrderController.java
@@ -1,11 +1,20 @@
 package com.s4r.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.s4r.business.service.order.OrderService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
+    private final OrderService orderService;
 
-    
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/place-order")
+    public void placeOrder(@RequestParam Long userId, @RequestParam Long orderId) {
+
+    }
+
 }
