b0411e27c7dab92908583d114cfe59db42a19bf7
saidurraahmaan <saidurraahmaan@gmail.com>
2024-06-20 18:35:23 +0600

    Fix image warning

--- plate-drop-ui/src/components/Header/index.tsx
+++ plate-drop-ui/src/components/Header/index.tsx
@@ -5,25 +5,23 @@ import { Header } from 'antd/es/layout/layout';
 import Image from 'next/image';
 import { Menu } from 'antd';
 import { getUnAuthorizedNavItems } from '@/app/utils';
+import logoImg from '../../../public/plate-drop-logo.png';
 import styles from './header.module.css';
 
 const AppHeader = () => {
   const router = useRouter();
   const navItems = getUnAuthorizedNavItems.map((ele) => ({
-    ...ele,
+    key: ele.key,
+    label: ele.label,
     onClick: () => router.push(ele.navigateTo),
   }));
+
   return (
     <>
       <Header style={{ display: 'flex', alignItems: 'center' }}>
         <div className={styles.nav_container}>
           <div className="d-flex">
-            <Image
-              src={'/plate-drop-logo.png'}
-              width={100}
-              height={60}
-              alt="logo"
-            />
+            <Image src={logoImg} width={100} height={60} alt="logo" priority />
           </div>
           <div>
             <Menu
