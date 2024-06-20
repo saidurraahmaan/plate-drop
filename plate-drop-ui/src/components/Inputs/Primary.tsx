401b8b2d0075970b2b905dda102276703f94fc08
saidurraahmaan <saidurraahmaan@gmail.com>
2024-06-20 14:10:14 +0600

    Create component

---
+++ plate-drop-ui/src/components/Inputs/Primary.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Input, InputProps } from 'antd';
+
+const PrimaryInput: React.FC<InputProps> = () => {
+  return <Input />;
+};
+
+export default PrimaryInput;
--- plate-drop-ui/src/components/index.ts
+++ plate-drop-ui/src/components/index.ts
@@ -1,3 +1,6 @@
 import AppHeader from './Header';
+import PasswordInput from './Inputs/Password';
+import PrimaryInput from './Inputs/Primary';
+import PrimaryButton from './Buttons/Primary';
 
-export { AppHeader };
+export { AppHeader, PrimaryInput, PasswordInput, PrimaryButton };
