b27199be9dba464b4cf802e26ae9f2ffb9ba6773
saidurraahmaan <saidurraahmaan@gmail.com>
2024-06-25 01:56:00 +0600

    fix warning

--- plate-drop-ui/src/components/Buttons/Primary.tsx
+++ plate-drop-ui/src/components/Buttons/Primary.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes, ReactNode } from 'react';
+import React from 'react';
 import { Button, ButtonProps } from 'antd';
 
 const PrimaryButton: React.FC<ButtonProps> = ({ children }) => {
