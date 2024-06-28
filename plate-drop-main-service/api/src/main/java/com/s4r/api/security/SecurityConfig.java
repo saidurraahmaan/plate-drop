210f3f9e3fab3cfa7c7981a5f465e3c39a167c0f
saidurraahmaan <saidurraahmaan@gmail.com>
2024-06-29 01:22:40 +0600

    Configure cors

--- plate-drop-main-service/api/src/main/java/com/s4r/api/security/SecurityConfig.java
+++ plate-drop-main-service/api/src/main/java/com/s4r/api/security/SecurityConfig.java
@@ -1,33 +1,48 @@
 package com.s4r.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
     private static final String[] WHITE_LIST_URL = {
-            "/auth/**",
+            "/api/auth/**",
             "/swagger-ui/**",
             "/swagger-resources/**",
             "/swagger-ui.html",
             "/v3/api-docs",
     };
     private final JwtService jwtService;
+    private final Environment env;
+    @Value("${security.cors-domains}")
+    private List<String> corsDomains;
 
-    public SecurityConfig(JwtService jwtService) {
+    public SecurityConfig(JwtService jwtService, Environment env) {
         this.jwtService = jwtService;
+        this.env = env;
     }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(corsCustomizer())
                 .authorizeHttpRequests((authorize) -> authorize.requestMatchers(WHITE_LIST_URL)
                         .permitAll()
                         .anyRequest()
@@ -45,6 +60,25 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private Customizer<CorsConfigurer<HttpSecurity>> corsCustomizer() {
+        if (!env.matchesProfiles("dev") || corsDomains == null || corsDomains.isEmpty()) {
+            return withDefaults();
+        }
+
+        return c ->
+        {
+            c.configurationSource(r ->
+            {
+                var cors = new CorsConfiguration();
+                cors.setAllowedOrigins(corsDomains);
+                cors.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE"));
+                cors.setAllowedHeaders(List.of("*"));
+                cors.setAllowCredentials(true);
+                return cors;
+            });
+        };
     }
 
 }
