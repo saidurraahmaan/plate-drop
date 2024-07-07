8edf34042cafa5c6d82db05f51395f2fbd340a02
saidurraahmaan <saidurraahmaan@gmail.com>
2024-07-08 01:28:31 +0600

    add-restaurant change

--- plate-drop-ui/src/constants/app-routes.ts
+++ plate-drop-ui/src/constants/app-routes.ts
@@ -4,7 +4,7 @@ const APP_ROUTES = {
   DELIVERY_REGISTRATION: '/auth/delivery/register',
   RESTAURANT_REGISTRATION: '/auth/restaurant/register',
   RESTAURANT_MANAGEMENT: '/restaurant-management',
-  ADD_RESTAURANT: '/restaurant-management/add/new',
+  ADD_RESTAURANT: '/restaurant-management/add-new',
   VIEW_RESTAURANT: '/restaurant-management/view/resturant',
   LOGIN: '/auth/login',
 };
