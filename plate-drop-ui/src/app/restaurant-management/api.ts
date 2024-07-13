ae236998cf37283c2b5a24016ad49b0da41a852d
saidurraahmaan <saidurraahmaan@gmail.com>
2024-07-14 02:16:46 +0600

    rewrite api

--- plate-drop-ui/src/app/restaurant-management/api.ts
+++ plate-drop-ui/src/app/restaurant-management/api.ts
@@ -1,20 +1,16 @@
-import fetchInstance from '@/libs/fetchApi';
-import { IRestaurant, IRestaurantFieldType } from './add-new/type';
+import fetchClient from '@/libs/fetchClient';
+import { IRestaurantFieldType } from './add-new/type';
 
 const BASE_URL = '/restaurant';
 
 const getEndpoint = (endpoint: string) => `${BASE_URL}${endpoint}`;
 
-const RESTAURANT_API = {
+export const RESTAURANT_API = {
   add: getEndpoint('/add'),
   delete: getEndpoint('/delete'),
   userRestaurants: getEndpoint('/all'),
 };
 
 export const addRestaurant = (data: IRestaurantFieldType) => {
-  return fetchInstance.post(RESTAURANT_API.add, data);
-};
-
-export const fetchUserRestaurants = () => {
-  return fetchInstance.get<IRestaurant[]>(RESTAURANT_API.userRestaurants);
+  return fetchClient.post(RESTAURANT_API.add, data);
 };
